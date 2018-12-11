0b25a523e08c6124d948800e2b5b92e3d209c1ba
nartydev <narty.dev@gmail.com>
2018-12-11 22:23:58 +0100

    :wrench: gulpfile.js > Define all tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const minify = require('gulp-minify-css');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 
 // Compile Sass & Inject Into Browser
 gulp.task('sass', function() {
     return gulp.src('assets/sass/*.scss')
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .pipe(gulp.dest("assets/css"))
+        .pipe(sass({
+          outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer())
+        .pipe(minify())
+        .pipe(concat('main.min.css'))
+        .pipe(gulp.dest("assets/css"))
+});
+
+gulp.task('js', function() {
+    return gulp.src(source + '/assets/js/*.js')
+        .pipe(uglify())
+        .pipe(concat('global.min.js'))
+        .pipe(gulp.dest('assets/js'));
 });
 
 
 // Watch Sass & Serve
 gulp.task('watch', function() {
-  gulp.watch('assets/sass/*.scss', gulp.series('sass'));
+  gulp.watch('assets/sass/*.scss', gulp.series('sass', 'js'));
 });
 
 // Default Task
