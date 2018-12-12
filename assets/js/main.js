3d11c37bd0dd41bf410819da88e4f9b639ddd6d0
majejam <t.f.m.lacroix@gmail.com>
2018-12-12 11:43:18 +0100

    :boom: added main.js

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const menuBurger = document.querySelector('.menu-burger')
+const menuContainer = document.querySelector('.side-menu-container')
+
+menuBurger.addEventListener('click', () => {
+  menuBurger.classList.toggle('menu-burger-open-animation')
+  menuBurger.classList.toggle('menu-burger-close-animation')
+  menuContainer.classList.toggle('menu-container-open')
+  menuContainer.classList.toggle('menu-container-close')
+})
