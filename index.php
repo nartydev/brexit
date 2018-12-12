58e3ee9aa247b09cfa50da0d42d90264cfaaf038
majejam <t.f.m.lacroix@gmail.com>
2018-12-12 10:43:37 +0100

    :boom: Added base for index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <title>Brexit</title>
+  <meta charset="utf-8">
+  <meta name="description" content="Immersive experience">
+  <meta property="og:title" content="Brexit ">
+  <meta property="og:type" content="website">
+  <meta property="og:image" content="#">
+  <meta property="og:description" content="Immersive Experience">
+  <meta property="og:site_name" content="Brexit">
+  <meta property="og:locale" content="fr_FR">
+  <link rel="shortcut icon" href="#">
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+
+</body>
+
+</html>
