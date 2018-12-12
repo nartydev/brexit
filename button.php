b6ce2b91d3e2d799fa2e7fff076fe644a199d43d
majejam <t.f.m.lacroix@gmail.com>
2018-12-12 17:33:55 +0100

    :tada finnished button

--- button.php
+++ button.php
@@ -3,4 +3,5 @@
   footer part. Use for whole site.
 */
 ?>
+<button type="button" class="main-btn arrow-btn arrow-left circle-btn"></button>
 <button type="button" class="main-btn arrow-btn arrow-right circle-btn"></button>
