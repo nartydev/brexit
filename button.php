c39929215029db32ef13c44ef526ac83dfa2f3e2
majejam <t.f.m.lacroix@gmail.com>
2018-12-13 13:26:24 +0100

    :boom: removed home page buttons

--- button.php
+++ button.php
@@ -1,7 +1,7 @@
 <?php
 /*
   Button part. Use for whole site.
+  <button type="button" class="main-btn arrow-btn arrow-left circle-btn"></button>
+  <button type="button" class="main-btn arrow-btn arrow-right circle-btn"></button>
 */
 ?>
-<button type="button" class="main-btn arrow-btn arrow-left circle-btn"></button>
-<button type="button" class="main-btn arrow-btn arrow-right circle-btn"></button>
