55c2ca7f1f6850b9a8d1e6ec83675ba85c7300bb
majejam <t.f.m.lacroix@gmail.com>
2018-12-13 10:28:38 +0100

    :bug: fixed some issues with the buttons

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
   <meta property="og:description" content="Immersive Experience">
   <meta property="og:site_name" content="Brexit">
   <meta property="og:locale" content="fr_FR">
-  <link rel="shortcut icon" href="#">
+  <link rel="shortcut icon" href="assets/img/favico-2.png">
   <link rel="stylesheet" type="text/css" href="assets/css/main.min.css?d=<?php echo rand(0,10000000000000000) ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:200,300,400,500,600,700|Montserrat:200,300,400,500,600,700|Noto+Serif+SC|Josefin+Sans:300,400,600,700" rel="stylesheet">
