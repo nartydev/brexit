2273cba48ec4e2061b564d22528eaa75e8e802a7
majejam <t.f.m.lacroix@gmail.com>
2018-12-15 19:47:06 +0100

    added qr code template & country selection

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,11 +39,17 @@ const landingContainer = document.querySelector('.landing-container')
 const beginButton = document.querySelector('.landing-begin-button')
 const landingTransitionContainer = document.querySelector('.landing-transition')
 const landingTranstionBars = document.querySelector('.landing-transition-bar')
+const qrTransitionContainer = document.querySelector('.qr-transition')
+const qrTranstionBars = document.querySelector('.qr-transition-bar')
 const allCountries = document.querySelectorAll('.countries-svg')
 const $buttons = document.querySelectorAll('.main-btn')
 const contentMap = document.querySelector('.content-map')
+const containerMap = document.querySelector('.container-map')
 const buttonCountriesLeft = document.querySelector('.responsive-btn-left')
 const buttonCountriesRight = document.querySelector('.responsive-btn-right')
+const confirmCountryBtn = document.querySelector('.confirm-country')
+const countryDisplay = document.querySelector('.country-name-display')
+const qrCode = document.querySelector('.inner-qr-code-container')
 let countryNum = 0;
 for(const $button of $buttons)
 {
@@ -81,6 +87,7 @@ buttonCountriesLeft.addEventListener('click', () => {
   }
   goToCountry(data.countries[countryNum].posx,data.countries[countryNum].posy,4)
   allCountries[data.countries[countryNum].svg_id].classList.add('countries-svg-selected')
+  getCountryName()
 })
 buttonCountriesRight.addEventListener('click', () => {
   clearClass()
@@ -91,11 +98,40 @@ buttonCountriesRight.addEventListener('click', () => {
   }
   goToCountry(data.countries[countryNum].posx,data.countries[countryNum].posy,4)
   allCountries[data.countries[countryNum].svg_id].classList.add('countries-svg-selected')
+  getCountryName()
 })
 
-for (var i = 0; i < allCountries.length; i++) {
+confirmCountryBtn.addEventListener('click', () => {
+  getCountryName()
+  qrTransitionContainer.classList.toggle('qr-transition-show')
+  qrTranstionBars.classList.toggle('qr-transition-bar-animation')
+  setTimeout(function(){
+    containerMap.style.display = "none"
+    qrCode.style.display = "flex"
+  }, 500);
+  setTimeout(function(){
+    //qrTransitionContainer.style.display = "none"
+    qrTransitionContainer.classList.toggle('qr-transition-show')
+  }, 1000);
+})
+
+function getCountryName(){
+  for (let i = 0; i < allCountries.length; i++) {
+    if(allCountries[i].classList.contains('countries-svg-selected')){
+      for (let j = 0; j < data.countries.length; j++) {
+        if(data.countries[j].svg_id == i){
+          countryDisplay.innerHTML = data.countries[j].name
+        }
+      }
+    }
+  }
+}
+
+for (let i = 0; i < allCountries.length; i++) {
   allCountries[i].addEventListener('click', () => {
-    console.log('hello')
+    clearClass()
+    allCountries[i].classList.add('countries-svg-selected')
+    getCountryName()
   })
 }
 
