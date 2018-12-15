1b6fe4ecb55d1f86e2af41b7034864753e95e64c
majejam <t.f.m.lacroix@gmail.com>
2018-12-15 15:49:28 +0100

    :bug: fixed block of hover and resize problem

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,27 @@ const data = {
     countries: [
       {
         name: "France",
+        svg_id: 1,
         posx: "82%",
         posy: "-28%"
       },{
         name: "Allemagne",
+        svg_id: 4,
         posx: "44%",
         posy: "-15%"
       },{
         name: "Espagne",
+        svg_id: 0,
         posx: "117%",
         posy: "-52%"
       },{
         name: "Italie",
+        svg_id: 3,
         posx: "40%",
         posy: "-52%"
       },{
         name: "UK",
+        svg_id: 2,
         posx: "94%",
         posy: "8%"
       }
@@ -45,6 +50,14 @@ for(const $button of $buttons)
     const button = new Button($button)
 }
 
+const resize = () => {
+  contentMap.removeAttribute("style")
+
+}
+// listen to resize
+window.addEventListener('resize', resize)
+resize()
+
 beginButton.addEventListener('click', () => {
   landingTransitionContainer.classList.toggle('landing-transition-show')
   landingTranstionBars.classList.toggle('landing-transition-bar-animation')
@@ -58,26 +71,36 @@ beginButton.addEventListener('click', () => {
 })
 
 buttonCountriesLeft.addEventListener('click', () => {
+  clearClass()
   if(countryNum == 0){
     countryNum = data.countries.length-1
   }else{
     countryNum--
   }
   goToCountry(data.countries[countryNum].posx,data.countries[countryNum].posy,4)
+  allCountries[data.countries[countryNum].svg_id].classList.add('countries-svg-selected')
 })
 buttonCountriesRight.addEventListener('click', () => {
+  clearClass()
   if(countryNum == data.countries.length-1){
     countryNum = 0
   }else{
     countryNum++
   }
   goToCountry(data.countries[countryNum].posx,data.countries[countryNum].posy,4)
+  allCountries[data.countries[countryNum].svg_id].classList.add('countries-svg-selected')
 })
 
 for (var i = 0; i < allCountries.length; i++) {
   allCountries[i].addEventListener('click', () => {
     console.log('hello')
   })
+}
+
+function clearClass(){
+  for (var i = 0; i < allCountries.length; i++) {
+    allCountries[i].classList.remove('countries-svg-selected')
+  }
 }
 
 function goToCountry(tposx, tposy, dscale){
