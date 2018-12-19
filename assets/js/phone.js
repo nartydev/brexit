84664e123b1042aa39e44615ffbec02c982e6311
majejam <t.f.m.lacroix@gmail.com>
2018-12-19 14:29:52 +0100

    :tada: added text question

--- assets/js/phone.js
+++ assets/js/phone.js
@@ -2,6 +2,8 @@ const socket = io.connect();
 const infoRound = document.querySelector('.info-round')
 const quizBox = [...document.querySelectorAll('.box-quiz')]
 const endPhone = document.querySelector('.end-phone')
+const answer = document.querySelectorAll('.word-quiz')
+console.log(answer)
 let idQuestion = 1
 let idSocket
 
@@ -32,8 +34,11 @@ infoRound.addEventListener('click', _event => {
 })
 
 socket.on('questionValue', _data => {
+    //console.log(_data.questions[idQuestion].yes,_data.questions[idQuestion].no)
     if (idQuestion < 10 && idSocket == _data.idSocket) {
         idQuestion++
+        answer[0].innerHTML = _data.questions[idQuestion-1].yes
+        answer[1].innerHTML = _data.questions[idQuestion-1].no
         numberQ.innerHTML = _data.data.id + 1
     } else {
         idQuestion++
